9b9cfbe92d20c48b25948b313be3c5270cc214df
chen <cycdanielboi@gmail.com>
2023-06-17 21:30:40 +0900

    add frontend env

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 //queries and mutations along with the loading and error state
 const mainApi = createApi({
   reducerPath: "mainApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.API_BASE_URL }),
   tagTypes: ["Kpis", "products", "transactions"],
   endpoints: () => ({}),
 });
